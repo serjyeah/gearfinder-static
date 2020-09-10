dad427c4a1d57825f5e756a5d88ce8efee9b90de
Sergiy Shaklein <serjshaklein@gmail.com>
2020-09-10 20:47:23 +0300

    initial commit

---
+++ precache-manifest.7ce1916906f0e3b43c2cba52d83242fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fcac51804661405ca3cdb22b86071d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ad2769c2cf7464dfd7f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6efd21606178ccb2fed5",
+    "url": "/static/js/2.a169ae28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a169ae28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ad2769c2cf7464dfd7f",
+    "url": "/static/js/main.3338ed68.chunk.js"
+  },
+  {
+    "revision": "390798c437314ad04679",
+    "url": "/static/js/runtime-main.b1e70d69.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
